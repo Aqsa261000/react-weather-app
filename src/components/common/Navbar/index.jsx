16fc928f7752d8d44d8f76e3d17b762f422459d4
Aqsa261000 <aqsamehdiali26@gmail.com>
2026-04-20 00:08:07 +0500

    nav logo added

--- src/components/common/Navbar/index.jsx
+++ src/components/common/Navbar/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Logo } from '../../../assets'
 
 const Navbar = () => {
   return (
-    <div className='text-white font-bold h-12 flex items-center justify-center text-xl gap-2'><img src="../../../../public/favicon.png" alt="nav-icon" className='w-10 h-10' />Weather Check</div>
+    <div className='text-white font-bold h-12 flex items-center justify-center text-xl gap-2'><img src={Logo} alt="nav-icon" className='w-10 h-10' />Weather Check</div>
   )
 }
 
